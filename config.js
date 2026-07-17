21618b3fb260a318b28a1f7b5d3e1b067da7e386
Neo Rysik <neorysik@gmail.com>
2026-07-16 20:22:01 -0400

    Connect chapter signup to MailerLite

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.SITE_CONFIG = {
   // Paste the HTTPS form action from MailerLite's embedded form code.
   // Example shape: https://assets.mailerlite.com/jsonp/ACCOUNT/forms/FORM/subscribe
-  mailerLiteFormAction: ""
+  mailerLiteFormAction: "https://assets.mailerlite.com/jsonp/2512787/forms/193196965479056785/subscribe"
 };
